030b205fc8ee7243b06ecafa678294a8ecad7821
Stark <serj.elessar@gmail.com>
2022-09-14 19:38:29 +0300

    feat: add catalog search

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const catalogWrapper = document.querySelector('.catalog__list')
-  const favoritesBtn = document.querySelector('.product-card__favorites')
-  const addToCartBtn = document.querySelector('.product-card__add-cart')
+  const searchPanel = document.querySelector('input[name="search-input"]')
+  const allProducts = document.querySelectorAll('.catalog__item')
 
   catalogWrapper.addEventListener('click', (event) => {
     const target = event.target
@@ -12,5 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
     if (target && target.closest('.product-card__add-cart')) {
       target.closest('.product-card__add-cart').classList.toggle('active')
     }
+  })
+
+  searchPanel.addEventListener('input', function (e) {
+    const searchValue = this.value.toLowerCase()
+
+    allProducts.forEach((product) => {
+      const productTitle = product.querySelector('.product-card__title').innerText.toLowerCase()
+
+      if (searchValue !== '') {
+        product.style.display = productTitle.indexOf(searchValue) > -1
+          ? 'block'
+          : 'none'
+      } else {
+        product.style.display = 'block'
+      }
+    })
   })
 })
